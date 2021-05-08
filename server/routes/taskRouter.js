1d9926abab864721b249863323109ed61ba2b1e1
David Vinson <davidvinson018@gmail.com>
2021-05-07 21:51:09 -0500

    POST endpt works as expected

--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -4,6 +4,25 @@ const pool = require('../modules/pool.js');
 
 //endpoints
 
+router.post('/', (req, res) => {
+    //route to a new task
+    console.log(req.body);
+    // console.log(req.params);
+    
+    let task = req.body;
+    let queryText = `INSERT INTO "tasks" ("task_name")
+    VALUES ($1);`;
+    pool.query(queryText, [task.task_name])
+    .then((result) => {
+        console.log(result);
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log(`the error: ${error}`);
+        res.sendStatus(500);
+    })
+});
+
 router.get('/', (req, res) => {
     console.log('get tasks here');
     const queryText = `SELECT * FROM "tasks";`;
